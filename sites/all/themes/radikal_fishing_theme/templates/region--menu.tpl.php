510453a29ed8a1c5ef83182acb77380f09d3975b
Dani-Server <danigilkp@gmail.com>
2014-09-14 18:59:16 +0200

    Updated region--menu.tpl.php and some css.

--- sites/all/themes/radikal_fishing_theme/templates/region--menu.tpl.php
+++ sites/all/themes/radikal_fishing_theme/templates/region--menu.tpl.php
@@ -5,7 +5,7 @@
       <!--<div class="primary-menu inline">-->
         <?php // print theme('links__system_main_menu', array('prefix' => '<div>', 'links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'main-menu')), 'heading' => array('text' => t('Main menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
 <!--      </div>
-      --><div class="second-menu inline"> 
+      --><div class="second-menu block"> 
        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'secondary-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
       </div>
       <?php // print theme('links__system_product_category_menu', array('links' => $product_category, 'attributes' => array('id' => 'product-category', 'class' => array('links', 'inline', 'clearfix', 'product-category')), 'heading' => array('text' => t('Product category menu'),'level' => 'h2','class' => array('element-invisible')))); ?>

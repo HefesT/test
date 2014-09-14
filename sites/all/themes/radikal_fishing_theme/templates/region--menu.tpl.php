1cd2bfe920364aa1a67031999d52cdfc144ec08a
Dani-Server <danigilkp@gmail.com>
2014-09-14 18:32:54 +0200

    Added secondary menu on region--menu.tpl.php for store admin reports links.

--- sites/all/themes/radikal_fishing_theme/templates/region--menu.tpl.php
+++ sites/all/themes/radikal_fishing_theme/templates/region--menu.tpl.php
@@ -1,15 +1,18 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
     <?php // if ($main_menu || $secondary_menu): ?>
-    <!--<nav class="navigation">-->
+    <nav class="navigation">
       <!--<div class="primary-menu inline">-->
         <?php // print theme('links__system_main_menu', array('prefix' => '<div>', 'links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'main-menu')), 'heading' => array('text' => t('Main menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
 <!--      </div>
--->      <div class="second-menu inline">
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'secondary-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
-      </div><!--
-    </nav>-->
+      --><div class="second-menu inline"> 
+       <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'secondary-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
+      </div>
+      <?php // print theme('links__system_product_category_menu', array('links' => $product_category, 'attributes' => array('id' => 'product-category', 'class' => array('links', 'inline', 'clearfix', 'product-category')), 'heading' => array('text' => t('Product category menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
+        <?php print $content; ?>
+      
+    </nav>
     <?php // endif; ?>
-    <?php print $content; ?>
+    <?php // print $content; ?>
   </div>
 </div>
